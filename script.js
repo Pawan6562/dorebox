8c4ae90656ac9da35f024cfe5c34ab6ca87b30a9
Pawan6562 <psgamingff342@gmail.com>
2025-08-13 23:55:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     
-    // Your movie database
+    // Your COMPLETE movie database (All 28 movies are here now)
     const movies = [
       { title: "Chronicle of the Moon", url: "https://smallshorts.com/NobitaChronicleoftheMoon", poster: "https://i.postimg.cc/BbmtZs0X/m3.jpg" },
       { title: "Sky Utopia", url: "https://t.me/doremonallmoviesepisodes/2019", poster: "https://i.postimg.cc/Nf3QTNXq/doraemon-movie-nobitas-sky-utopia-in-hindi.jpg" },
@@ -13,8 +13,23 @@ document.addEventListener('DOMContentLoaded', () => {
       { title: "Steel Troops – New Age", url: "https://t.me/doremonallmoviesepisodes/2048", poster: "https://i.postimg.cc/43C9KJr0/Doraemon-The-Movie-Nobita-and-the-Steel-Troops.jpg" },
       { title: "Three Visionary Swordsmen", url: "https://t.me/doremonallmoviesepisodes/2051", poster: "https://i.postimg.cc/RZ82qxJ3/Doraemon-The-Movie-Nobita-s-Three-Magical-Swordsmen.png" },
       { title: "Nobita In Hara Hara Planet", url: "https://t.me/doremonallmoviesepisodes/2055", poster: "https://i.postimg.cc/RV6bNkLK/Doraemon-The-Movie-Nobitain-Hara-Hara-Planet-bycjh.jpg" },
-      { title: "Adventure of Koya Koya", url: "https://t.me/doremonallmoviesepisodes/2061", poster: "https://i.postimg.cc/DwCg656n/Adventures-of-Koya-Koya-Planet-Movie-by-cjh.png" }
-      // ... baaki movies yahan add karein
+      { title: "Adventure of Koya Koya", url: "https://t.me/doremonallmoviesepisodes/2061", poster: "https://i.postimg.cc/DwCg656n/Adventures-of-Koya-Koya-Planet-Movie-by-cjh.png" },
+      { title: "Doraemon nobita and the Birthday of japan", url: "https://t.me/doremonallmoviesepisodes/2063", poster: "https://i.postimg.cc/MKqNrP7Q/Doraemon-The-Movie-Nobita-and-the-birth-of-Japan.jpg" },
+      { title: "Nobita's Dinosaur", url: "https://t.me/doremonallmoviesepisodes/2065", poster: "https://i.postimg.cc/5NKgqnn1/Doraemon-Nobitas-new-dinosaur-by-cjh.jpg" },
+      { title: "Parallel Visit to West", url: "https://t.me/doremonallmoviesepisodes/2067", poster: "https://i.postimg.cc/prbYFGHC/Doraemon-Nobita-Bana-Superhero-Hindi-by-cjh.jpg" },
+      { title: "Legend of Sun King", url: "https://t.me/doremonallmoviesepisodes/2070", poster: "https://i.postimg.cc/mrQ7v7Qd/Doraemon-nobita-and-the-legend-of-sun-king-by-cjh.jpg" },
+      { title: "Stand by Me – Part 1", url: "https://t.me/doremonallmoviesepisodes/2071", poster: "https://i.postimg.cc/vmkLDN1X/Doraemon-The-Movie-Stand-by-Me-by-cjh.png" },
+      { title: "Stand by Me – Part 2", url: "https://t.me/doremonallmoviesepisodes/2072", poster: "https://i.postimg.cc/y8wkR4PJ/Doraemon-The-Movie-Stand-by-Me-2-by-cjh.png" },
+      { title: "Doraemon Nobita's Great Adventure in the South Seas", url: "https://t.me/doremonallmoviesepisodes/2152", poster: "https://i.postimg.cc/8zC06x5V/Nobita-Great-Adventure-to-the-South-Seas-by-cjh.jpg" },
+      { title: "Khilone Ki Bhul Bhulaiya", url: "https://t.me/doremonallmoviesepisodes/2095", poster: "https://i.postimg.cc/w38qYR5V/Doraemon-Khel-Khilona-Bhool-Bhulaiya-by-cjh.jpg" },
+      { title: "Doraemon The Movie – Nobita and the Underwater Adventure", url: "https://t.me/doremonallmoviesepisodes/2120", poster: "https://i.postimg.cc/VLgfVvrj/Doraemon-The-Movie-Nobita-and-the-Underwater-Adventure.jpg" },
+      { title: "Birdopia Ka Sultan", url: "https://t.me/doremonallmoviesepisodes/2129", poster: "https://i.postimg.cc/hjVgbtRQ/Doraemon-The-Movie-Nobita-Aur-Birdopia-Ka-Sultan.jpg" },
+      { title: "Doraemon Nobita's Treasure Island", url: "https://t.me/doremonallmoviesepisodes/2138", poster: "https://i.postimg.cc/t46rgZ36/Doraemon-the-Nobita-s-Treasure-Island-by-cjh.jpg" },
+      { title: "Doraemon The Movie Nobita The Explorer Bow Bow", url: "https://t.me/doremonallmoviesepisodes/2150", poster: "https://i.postimg.cc/HxY336f0/The-Movie-Nobita-The-Explorer-Bow-Bow-by-cjh.png" },
+      { title: "Doraemon Nobita and the Windmasters", url: "https://t.me/doremonallmoviesepisodes/2154", poster: "https://i.postimg.cc/bYFLHHLb/Doraemon-Toofani-Adventure-by-cjh.jpg" },
+      { title: "Doraemon Nobita and the Island of Miracle", url: "https://t.me/doremonallmoviesepisodes/2158", poster: "https://i.postimg.cc/yd8X0kZv/Doraemon-The-Movie-Nobita-Aur-Jadooi-Tapu-by-cjh.jpg" },
+      { title: "Doraemon Galaxy Super Express Hindi", url: "https://t.me/doremonallmoviesepisodes/2165", poster: "https://i.postimg.cc/XY6fQ25Z/Doraemon-The-Movie-Galaxy-Super-Express-by-cjh.png" }, 
+      { title: "Doraemon Nobita And The Kingdom Of Robot Singham", url: "https://t.me/doremonallmoviesepisodes/2174", poster: "https://i.postimg.cc/j5fNHPj6/The-Movie-Nobita-and-the-Kingdom-of-Robot-by-cjh.jpg" }
     ];
 
     // --- AAPKA AD CODE YAHAN HAI ---
