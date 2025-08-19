77d3fbf96a8cddab97a517889694549b2a63edc4
Pawan6562 <psgamingff342@gmail.com>
2025-08-19 18:58:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,41 @@
 document.addEventListener("DOMContentLoaded", () => {
+    // Yahan hum apni saari movies ki list banayenge
     const movies = [
-      { title: "Chronicle of the Moon", url: "https://smallshorts.com/NobitaChronicleoftheMoon", poster: "https://i.postimg.cc/BbmtZs0X/m3.jpg" },
-      { title: "Sky Utopia", url: "https://t.me/doremonallmoviesepisodes/2019", poster: "https://i.postimg.cc/Nf3QTNXq/doraemon-movie-nobitas-sky-utopia-in-hindi.jpg" },
-      { title: "Antarctic Adventure", url: "https://t.me/doremonallmoviesepisodes/2024", poster: "https://i.postimg.cc/9f4SpVHL/Doraemon-Nobita-Chal-Pada-Antarctica-Hindi-by-cjh.jpg" },
-      { title: "Doraemon jadoo Mantar aur jhanoom", url: "https://t.me/doremonallmoviesepisodes/2037", poster: "https://i.postimg.cc/Z5t0TfkP/Doraemon-The-Movie-Jadoo-Mantar-Aur-Jahnoom-by-cjh.jpg" },
-      { title: "Little Space War", url: "https://t.me/doremonallmoviesepisodes/2038", poster: "https://i.postimg.cc/wTt8Th7t/Doraemon-in-Nobitas-Little-Space-War-Movie-Hindi-Tamil-Telugu-Download-HD-jpg-990x557.png" },
-      { title: "Gadget Museum Ka Rahasya", url: "https://t.me/doremonallmoviesepisodes/2041", poster: "https://i.postimg.cc/9QsfxJbw/Doraemon-Gadget-Museum-Ka-Rahasya-by-cjh.jpg" },
-      { title: "Doraemon: Nobita's New Dinosaur (fan Dubbed)", url: "https://t.me/doremonallmoviesepisodes/2043", poster: "https://i.postimg.cc/hG0HJGX4/Doraemon-Nobitas-new-dinosaur-by-cjh.jpg" },
-      { title: "Space Hero", url: "https://t.me/doremonallmoviesepisodes/2045", poster: "https://i.postimg.cc/50CKBN0F/Doraemon-The-Movie-Nobita-aur-Antarishk-Daku-bycjh.jpg" },
-      { title: "Steel Troops – New Age", url: "https://t.me/doremonallmoviesepisodes/2048", poster: "https://i.postimg.cc/43C9KJr0/Doraemon-The-Movie-Nobita-and-the-Steel-Troops.jpg" },
-      { title: "Three Visionary Swordsmen", url: "https://t.me/doremonallmoviesepisodes/2051", poster: "https://i.postimg.cc/RZ82qxJ3/Doraemon-The-Movie-Nobita-s-Three-Magical-Swordsmen.png" },
-      { title: "Nobita In Hara Hara Planet", url: "https://t.me/doremonallmoviesepisodes/2055", poster: "https://i.postimg.cc/RV6bNkLK/Doraemon-The-Movie-Nobitain-Hara-Hara-Planet-bycjh.jpg" },
-      { title: "Adventure of Koya Koya", url: "https://t.me/doremonallmoviesepisodes/2061", poster: "https://i.postimg.cc/DwCg656n/Adventures-of-Koya-Koya-Planet-Movie-by-cjh.png" },
-      { title: "Doraemon nobita and the Birthday of japan", url: "https://t.me/doremonallmoviesepisodes/2063", poster: "https://i.postimg.cc/MKqNrP7Q/Doraemon-The-Movie-Nobita-and-the-birth-of-Japan.jpg" },
-      { title: "Nobita's Dinosaur", url: "https://t.me/doremonallmoviesepisodes/2065", poster: "https://i.postimg.cc/5NKgqnn1/Doraemon-Nobitas-new-dinosaur-by-cjh.jpg" },
-      { title: "Parallel Visit to West", url: "https://t.me/doremonallmoviesepisodes/2067", poster: "https://i.postimg.cc/prbYFGHC/Doraemon-Nobita-Bana-Superhero-Hindi-by-cjh.jpg" },
-      { title: "Legend of Sun King", url: "https://t.me/doremonallmoviesepisodes/2070", poster: "https://i.postimg.cc/mrQ7v7Qd/Doraemon-nobita-and-the-legend-of-sun-king-by-cjh.jpg" },
-      { title: "Stand by Me – Part 1", url: "https://t.me/doremonallmoviesepisodes/2071", poster: "https://i.postimg.cc/vmkLDN1X/Doraemon-The-Movie-Stand-by-Me-by-cjh.png" },
-      { title: "Stand by Me – Part 2", url: "https://t.me/doremonallmoviesepisodes/2072", poster: "https://i.postimg.cc/y8wkR4PJ/Doraemon-The-Movie-Stand-by-Me-2-by-cjh.png" },
-      { title: "Doraemon Nobita's Great Adventure in the South Seas", url: "https://t.me/doremonallmoviesepisodes/2152", poster: "https://i.postimg.cc/8zC06x5V/Nobita-Great-Adventure-to-the-South-Seas-by-cjh.jpg" },
-      { title: "Khilone Ki Bhul Bhulaiya", url: "https://t.me/doremonallmoviesepisodes/2095", poster: "https://i.postimg.cc/w38qYR5V/Doraemon-Khel-Khilona-Bhool-Bhulaiya-by-cjh.jpg" },
-      { title: "Doraemon The Movie – Nobita and the Underwater Adventure", url: "https://t.me/doremonallmoviesepisodes/2120", poster: "https://i.postimg.cc/VLgfVvrj/Doraemon-The-Movie-Nobita-and-the-Underwater-Adventure.jpg" },
-      { title: "Birdopia Ka Sultan", url: "https://t.me/doremonallmoviesepisodes/2129", poster: "https://i.postimg.cc/hjVgbtRQ/Doraemon-The-Movie-Nobita-Aur-Birdopia-Ka-Sultan.jpg" },
-      { title: "Doraemon Nobita's Treasure Island", url: "https://t.me/doremonallmoviesepisodes/2138", poster: "https://i.postimg.cc/t46rgZ36/Doraemon-the-Nobita-s-Treasure-Island-by-cjh.jpg" },
-      { title: "Doraemon The Movie Nobita The Explorer Bow Bow", url: "https://t.me/doremonallmoviesepisodes/2150", poster: "https://i.postimg.cc/HxY336f0/The-Movie-Nobita-The-Explorer-Bow-Bow-by-cjh.png" },
-      { title: "Doraemon Nobita and the Windmasters", url: "https://t.me/doremonallmoviesepisodes/2154", poster: "https://i.postimg.cc/bYFLHHLb/Doraemon-Toofani-Adventure-by-cjh.jpg" },
-      { title: "Doraemon Nobita and the Island of Miracle", url: "https://t.me/doremonallmoviesepisodes/2158", poster: "https://i.postimg.cc/yd8X0kZv/Doraemon-The-Movie-Nobita-Aur-Jadooi-Tapu-by-cjh.jpg" },
-      { title: "Doraemon Galaxy Super Express Hindi", url: "https://t.me/doremonallmoviesepisodes/2165", poster: "https://i.postimg.cc/XY6fQ25Z/Doraemon-The-Movie-Galaxy-Super-Express-by-cjh.png" }, 
-      { title: "Doraemon Nobita And The Kingdom Of Robot Singham", url: "https://t.me/doremonallmoviesepisodes/2174", poster: "https://i.postimg.cc/j5fNHPj6/The-Movie-Nobita-and-the-Kingdom-of-Robot-by-cjh.jpg" }
+        // Tumhari nayi movie, sabse upar!
+        { 
+            title: "Doraemon The Movie Nobita and the Underwater Adventure", 
+            poster: "https://i.postimg.cc/yYLjw5Pn/Doraemon-The-Movie-Nobita.jpg",
+            embed: `<IFRAME SRC="https://mivalyo.com/embed/2jjv05e2r19g" FRAMEBORDER=0 MARGINWIDTH=0 MARGINHEIGHT=0 SCROLLING=NO WIDTH=640 HEIGHT=360 allowfullscreen></IFRAME>`,
+            download: `https://mivalyo.com/download/2jjv05e2r19g` 
+        },
+        // Baaki movies ke liye bhi aise hi entry karni hai
+        { title: "Chronicle of the Moon", poster: "https://i.postimg.cc/BbmtZs0X/m3.jpg", embed: ``, download: `` },
+        { title: "Sky Utopia", poster: "https://i.postimg.cc/Nf3QTNXq/doraemon-movie-nobitas-sky-utopia-in-hindi.jpg", embed: ``, download: `` },
+        { title: "Antarctic Adventure", poster: "https://i.postimg.cc/9f4SpVHL/Doraemon-Nobita-Chal-Pada-Antarctica-Hindi-by-cjh.jpg", embed: ``, download: `` },
+        { title: "Doraemon jadoo Mantar aur jhanoom", poster: "https://i.postimg.cc/Z5t0TfkP/Doraemon-The-Movie-Jadoo-Mantar-Aur-Jahnoom-by-cjh.jpg", embed: ``, download: `` },
+        { title: "Little Space War", poster: "https://i.postimg.cc/wTt8Th7t/Doraemon-in-Nobitas-Little-Space-War-Movie-Hindi-Tamil-Telugu-Download-HD-jpg-990x557.png", embed: ``, download: `` },
+        { title: "Gadget Museum Ka Rahasya", poster: "https://i.postimg.cc/9QsfxJbw/Doraemon-Gadget-Museum-Ka-Rahasya-by-cjh.jpg", embed: ``, download: `` },
+        { title: "Doraemon: Nobita's New Dinosaur (fan Dubbed)", poster: "https://i.postimg.cc/hG0HJGX4/Doraemon-Nobitas-new-dinosaur-by-cjh.jpg", embed: ``, download: `` },
+        { title: "Space Hero", poster: "https://i.postimg.cc/50CKBN0F/Doraemon-The-Movie-Nobita-aur-Antarishk-Daku-bycjh.jpg", embed: ``, download: `` },
+        { title: "Steel Troops – New Age", poster: "https://i.postimg.cc/43C9KJr0/Doraemon-The-Movie-Nobita-and-the-Steel-Troops.jpg", embed: ``, download: `` },
+        { title: "Three Visionary Swordsmen", poster: "https://i.postimg.cc/RZ82qxJ3/Doraemon-The-Movie-Nobita-s-Three-Magical-Swordsmen.png", embed: ``, download: `` },
+        { title: "Nobita In Hara Hara Planet", poster: "https://i.postimg.cc/RV6bNkLK/Doraemon-The-Movie-Nobitain-Hara-Hara-Planet-bycjh.jpg", embed: ``, download: `` },
+        { title: "Adventure of Koya Koya", poster: "https://i.postimg.cc/DwCg656n/Adventures-of-Koya-Koya-Planet-Movie-by-cjh.png", embed: ``, download: `` },
+        { title: "Doraemon nobita and the Birthday of japan", poster: "https://i.postimg.cc/MKqNrP7Q/Doraemon-The-Movie-Nobita-and-the-birth-of-Japan.jpg", embed: ``, download: `` },
+        { title: "Nobita's Dinosaur", poster: "https://i.postimg.cc/5NKgqnn1/Doraemon-Nobitas-new-dinosaur-by-cjh.jpg", embed: ``, download: `` },
+        { title: "Parallel Visit to West", poster: "https://i.postimg.cc/prbYFGHC/Doraemon-Nobita-Bana-Superhero-Hindi-by-cjh.jpg", embed: ``, download: `` },
+        { title: "Legend of Sun King", poster: "https://i.postimg.cc/mrQ7v7Qd/Doraemon-nobita-and-the-legend-of-sun-king-by-cjh.jpg", embed: ``, download: `` },
+        { title: "Stand by Me – Part 1", poster: "https://i.postimg.cc/vmkLDN1X/Doraemon-The-Movie-Stand-by-Me-by-cjh.png", embed: ``, download: `` },
+        { title: "Stand by Me – Part 2", poster: "https://i.postimg.cc/y8wkR4PJ/Doraemon-The-Movie-Stand-by-Me-2-by-cjh.png", embed: ``, download: `` },
+        { title: "Doraemon Nobita's Great Adventure in the South Seas", poster: "https://i.postimg.cc/8zC06x5V/Nobita-Great-Adventure-to-the-South-Seas-by-cjh.jpg", embed: ``, download: `` },
+        { title: "Khilone Ki Bhul Bhulaiya", poster: "https://i.postimg.cc/w38qYR5V/Doraemon-Khel-Khilona-Bhool-Bhulaiya-by-cjh.jpg", embed: ``, download: `` },
+        { title: "Birdopia Ka Sultan", poster: "https://i.postimg.cc/hjVgbtRQ/Doraemon-The-Movie-Nobita-Aur-Birdopia-Ka-Sultan.jpg", embed: ``, download: `` },
+        { title: "Doraemon Nobita's Treasure Island", poster: "https://i.postimg.cc/t46rgZ36/Doraemon-the-Nobita-s-Treasure-Island-by-cjh.jpg", embed: ``, download: `` },
+        { title: "Doraemon The Movie Nobita The Explorer Bow Bow", poster: "https://i.postimg.cc/HxY336f0/The-Movie-Nobita-The-Explorer-Bow-Bow-by-cjh.png", embed: ``, download: `` },
+        { title: "Doraemon Nobita and the Windmasters", poster: "https://i.postimg.cc/bYFLHHLb/Doraemon-Toofani-Adventure-by-cjh.jpg", embed: ``, download: `` },
+        { title: "Doraemon Nobita and the Island of Miracle", poster: "https://i.postimg.cc/yd8X0kZv/Doraemon-The-Movie-Nobita-Aur-Jadooi-Tapu-by-cjh.jpg", embed: ``, download: `` },
+        { title: "Doraemon Galaxy Super Express Hindi", poster: "https://i.postimg.cc/XY6fQ25Z/Doraemon-The-Movie-Galaxy-Super-Express-by-cjh.png", embed: ``, download: `` }, 
+        { title: "Doraemon Nobita And The Kingdom Of Robot Singham", poster: "https://i.postimg.cc/j5fNHPj6/The-Movie-Nobita-and-the-Kingdom-of-Robot-by-cjh.jpg", embed: ``, download: `` }
     ];
 
     const movieGrid = document.getElementById("movie-grid");
@@ -36,25 +44,20 @@ document.addEventListener("DOMContentLoaded", () => {
 
     function displayMovies(movieArray) {
         movieGrid.innerHTML = "";
-        if (movieArray.length === 0) {
-            noResults.classList.remove("hidden");
-        } else {
-            noResults.classList.add("hidden");
-        }
-        
-        const adCode = `
-            <div id="frame" style="width: 300px;margin: auto;z-index: 99998;height: auto">
-                <iframe data-aa='2407034' src='//ad.a-ads.com/2407034/?size=300x250'
-                                style='border:0; padding:0; width:300px; height:250px; overflow:hidden;display: block;margin: auto'></iframe>
-            </div>
-        `;
+        noResults.classList.toggle("hidden", movieArray.length === 0);
 
-        movieArray.forEach((movie, index) => {
+        movieArray.forEach(movie => {
+            // Agar movie ka embed code nahi hai, to use mat dikhao
+            if (!movie.embed || !movie.download) return;
+
             const movieCard = document.createElement("div");
             movieCard.className = "movie-card";
 
+            // Har movie ke liye watch page ka link banao
+            const watchPageUrl = `watch.html?embed=${encodeURIComponent(movie.embed)}&title=${encodeURIComponent(movie.title)}&download=${encodeURIComponent(movie.download)}`;
+
             movieCard.innerHTML = `
-                <a href="${movie.url}" target="_blank">
+                <a href="${watchPageUrl}">
                     <div class="poster-container">
                         <img src="${movie.poster}" alt="${movie.title}" loading="lazy">
                         <div class="play-icon">
@@ -65,21 +68,14 @@ document.addEventListener("DOMContentLoaded", () => {
                 </a>
             `;
             movieGrid.appendChild(movieCard);
-
-            if ((index + 1) % 4 === 0) {
-                const adCard = document.createElement("div");
-                adCard.className = "ad-card";
-                adCard.innerHTML = adCode;
-                movieGrid.appendChild(adCard);
-            }
         });
     }
 
     searchBar.addEventListener("keyup", (e) => {
         const searchTerm = e.target.value.toLowerCase();
-        const filteredMovies = movies.filter(movie => {
-            return movie.title.toLowerCase().includes(searchTerm);
-        });
+        const filteredMovies = movies.filter(movie => 
+            movie.title.toLowerCase().includes(searchTerm)
+        );
         displayMovies(filteredMovies);
     });
 
