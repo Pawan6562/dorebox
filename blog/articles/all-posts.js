02d3483a3b856154da0c8581542d8fd49690f85a
Pawan6562 <psgamingff342@gmail.com>
2025-11-07 16:20:37 +0530

    Create all-posts.js to manage blog post objects
    This file aggregates all blog post objects into a single array for easier management and import.

---
+++ blog/articles/all-posts.js
@@ -0,0 +1,10 @@
+// Ye file saare post objects ko ek 'blogPosts' array mein jodti hai.
+// Jab bhi naya post banao, uski file yahan import karo aur use array mein add karo.
+
+// IMPORTANT: Ye saare script tags tumhare post.html aur index.html mein add honge.
+
+const blogPosts = [
+    nobitaLifestyle,
+    // Yahan naye post ka variable add karo, jaise:
+    // anotherPost,
+];
