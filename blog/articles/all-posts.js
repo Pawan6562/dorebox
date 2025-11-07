e1b7bcd3ffef9ab20f7c545c5ea7248396e73286
Pawan6562 <psgamingff342@gmail.com>
2025-11-07 21:14:50 +0530

    Delete blog/articles/all-posts.js

--- blog/articles/all-posts.js
+++
@@ -1,9 +0,0 @@
-// Step 1: Ek khali array banao.
-const blogPosts = [];
-
-// Step 2: Ek function banao jo is array mein post add karega.
-function addPost(postObject) {
-    if (postObject) {
-        blogPosts.push(postObject);
-    }
-}
