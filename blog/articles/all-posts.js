2b2b652d8f3cb4ca87ecab50100f99329a81a485
Pawan6562 <psgamingff342@gmail.com>
2025-11-07 16:28:04 +0530

    Refactor blogPosts initialization and addPost function
    Refactor blogPosts array initialization and add function.

--- blog/articles/all-posts.js
+++ blog/articles/all-posts.js
@@ -1,10 +1,9 @@
-// Ye file saare post objects ko ek 'blogPosts' array mein jodti hai.
-// Jab bhi naya post banao, uski file yahan import karo aur use array mein add karo.
+// Step 1: Ek khali array banao.
+const blogPosts = [];
 
-// IMPORTANT: Ye saare script tags tumhare post.html aur index.html mein add honge.
-
-const blogPosts = [
-    nobitaLifestyle,
-    // Yahan naye post ka variable add karo, jaise:
-    // anotherPost,
-];
+// Step 2: Ek function banao jo is array mein post add karega.
+function addPost(postObject) {
+    if (postObject) {
+        blogPosts.push(postObject);
+    }
+}
